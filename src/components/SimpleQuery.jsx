d3ac976db5b06d7734699ae744b6d87e601b698f
Ashish Panchal <ashish.panchal@yudiz.com>
2023-06-29 10:08:32 +0530

    first two simple query

---
+++ src/components/SimpleQuery.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LAUNCHES = gql`
+  query {
+    launches(limit: 10) {
+      mission_name
+    }
+  }
+`;
+
+function SimpleUseQuery() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error)
+  return (
+    <div>
+      <p>Error : {error}</p>
+    </div>
+  );
+
+  return (
+    <div>
+        <h1>Simple useQuery</h1>
+      <ul>
+        {data?.launches?.map((launch) => (
+          <li key={launch?.mission_name}>{launch?.mission_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SimpleUseQuery;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+
+const client = new ApolloClient({
+  uri: 'https://spacex-production.up.railway.app/graphql',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
